93b5ad651c1602f3d89743657c2bd6bc82e31bd2
orrin <orrin.rhodes@outlook.com>
2022-11-13 05:53:02 -0600

    sdasd32

--- javascript/higher.js
+++ javascript/higher.js
@@ -1,5 +1,27 @@
 console.log('hi');
 
+var slimes = [];
+
+function draw(amt){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle ='beige';
+    ctx.font = "50px Arial";
+    ctx.fillText('cliques',50,70);
+    
+    for (i=1; i < amt+1; i++){
+        var slime = drawSlime(20);
+        slimes.push(
+            {slime:slime,name:'bob',personality:{emo:0,coolkid:0}},
+            // {slime:slime,name:'bob',personality:{emo:12,coolkid:100}},
+            // {slime:slime2,name:'georgia',personality:{emo:65,misandrist:100}}
+        );
+    };
+};
+
+function update(slimes){
+
+};
+
 function spawnElem(elem,prop){
     var newElem = document.createElement(elem);
     var newProp = document.createTextNode('hi');
@@ -8,11 +30,9 @@ function spawnElem(elem,prop){
 
     if (elem == 'canvas'){
         if (newElem.getContext) {
-            
-            newElem.style.backgroundColor = 'black';
+            newElem.style.backgroundColor = 'grey';
             newElem.height = 500;
             newElem.width = 500;
-
             document.body.appendChild(newElem);
         };
     };
@@ -22,22 +42,39 @@ function spawnElem(elem,prop){
     return newElem;
 };
 
-function spawnSlime(cnvs,x,y,diam){
-    var ctx = cnvs.getContext('2d');
-    ctx.font = '50px Comic Sans MS';
-    ctx.fillStyle = 'green'
-    ctx.fillText('cliques', 10, 50);
+var canvas = spawnElem('canvas');
+var ctx = canvas.getContext('2d');
 
-    // ctx.fillRect(25, 25, 100, 100);
-    // x,y relative to canvas != body
-    var circle = new Path2D();
-    circle.arc(x, y, diam, 0, Math.PI * 2); // circle
-    ctx.fill(circle);
+function drawSlime(diam){
+    var x = Math.random()*canvas.width;
+    var y = Math.random()*canvas.height;
+    
+    ctx.fillStyle = 'green';
+
+    // var circle = new Path2D();
+    ctx.beginPath();
+    ctx.arc(x,y,diam,0,Math.PI * 2); // circle
+    ctx.closePath();
+    ctx.fill();
+    
+    return ctx;
 };
 
 var div1 = spawnElem('div');
-var canvas = spawnElem('canvas');
+draw(5);
 
+console.warn('-'.repeat(75));
 
-spawnSlime(canvas,100,150,20);
-spawnSlime(canvas,200,300,20);
+function read(){
+    console.table(slimes);
+    slimes.forEach(function(slime){
+    var personality = slime.personality;
+    for (const [attribute,value] of Object.entries(personality)) {
+        // console.log(attribute,value);
+    };
+    console.warn(slime.name);
+    console.table(personality)
+});
+};
+
+read();
