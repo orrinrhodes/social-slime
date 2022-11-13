f6cb620ab5663f7c38a2124a259a53a2f6b3a8a4
orrin <orrin.rhodes@outlook.com>
2022-11-13 07:09:08 -0600

    Update logger.js

--- javascript/logger.js
+++ javascript/logger.js
@@ -1,10 +1,10 @@
 var old = console.log;
 var logger = document.getElementById('log');
 console.log = function () {
-for (var i = 0; i < arguments.length; i++) {
-    if (typeof arguments[i] == 'object') {
+for (var i = 0; i < arguments.length; i++){
+    if (typeof arguments[i] == 'object'){
         logger.innerHTML += `${(JSON && JSON.stringify ? JSON.stringify(arguments[i],undefined,2):arguments[i])}<br />`;
-    } else {
+    } else{
         logger.innerHTML += `${arguments[i]}<br/>`;
     };
     };
