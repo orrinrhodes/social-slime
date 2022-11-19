64419c2547c963dcab183e27562e9d849278f537
orrin <orrin.rhodes@outlook.com>
2022-11-19 13:07:17 -0600

    sdas

--- javascript/json.js
+++ javascript/json.js
@@ -10,7 +10,36 @@ function readRaw(file, callback) {
     raw.send(null);
 }
 
-readRaw('./json/accounts.json', function(json){
+const url = 'http://ip-api.com/json';
+
+var ips = {
+    'fakeIP':{'actions':0,'limit':20}
+};
+
+var limit = 20;
+
+var delay = async (ms = 1000) => new Promise(handler => setTimeout(handler,ms));
+
+async function waitLoop(ip,length,code,ms) {
+    for (var i = 0; i < length; i++) {
+        if (ip.actions>=limit) {
+            console.log('limit reached!');
+            console.table(ips);
+            break;
+        } else {
+            ip.actions+= 1;
+            eval(code);
+        }
+        console.log('current actions: '+ip.actions);
+        await delay(ms);
+    };
+};
+
+readRaw(url, function(json){
     var data = JSON.parse(json);
-    console.table(data.accounts);
+    ips[data.query] = {'actions':0,'limit':20,};
+    console.table(ips);
+    var ip = ips[data.query];
+    console.log(`${data.query}'s limit: ${ip.limit}`);
+    waitLoop(ip,Infinity,``,250);
 });
